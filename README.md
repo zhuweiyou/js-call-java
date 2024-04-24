fc4ac91af6de8140648da86bdbf7b2bd1d98310d
zhuweiyou <i@zhuweiyou.com>
2024-04-24 16:07:32 +0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-call-java
+
+nodejs 调用 java 的 jar 示例
--- jsoup.js
+++ jsoup.js
@@ -4,10 +4,3 @@ const Jsoup = java.import('org.jsoup.Jsoup')
 
 const doc = Jsoup.connect("https://www.baidu.com/").get()
 console.log(doc.title());
-
-const newsHeadlines = doc.select("*");
-// console.log(Object.keys(newsHeadlines));
-for (let i = 0; i < newsHeadlines.size(); i++) {
-    const headline = newsHeadlines.get(i)
-    console.log(headline.attr("title"), headline.absUrl("href"));
-}
